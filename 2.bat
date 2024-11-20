91cfb1cea967280a7eb994ae92870091372fe634
xxyq <48348002+qq1790537742@users.noreply.github.com>
2024-11-20 21:55:54 +0800

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,2 +1,2 @@
 set "originalPath=%CD%"
-start cmd /k "cd .\app\CentralController && %originalPath%\Python310\python.exe -m ApplicationFramework.main"
+start cmd /k "cd .\app\CentralController && python -m ApplicationFramework.main"
